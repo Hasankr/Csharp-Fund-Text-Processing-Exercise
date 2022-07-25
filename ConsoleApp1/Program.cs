b888b29381a06415d0b75f510802d6b9803297e7
hasankr <hasankr@abv.bg>
2022-07-25 23:11:51 +0300

    added 8

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            double sum = 0;
+            foreach (var item in input)
+            {
+                char firstLetter = item[0];
+                char lastLetter = item[item.Length - 1];
+                double number = double.Parse(item.Substring(1, item.Length - 2));
+                double result = 0;
+                
+                
+                if (firstLetter >= 65 && firstLetter <= 90)
+                {
+                    int firstLtterPositionInTheAlphabet = firstLetter - 64;
+                    result = number / firstLtterPositionInTheAlphabet;
+                }
+                else
+                {
+                     int firstLtterPositionInTheAlphabet = firstLetter - 96;
+                    result = number * firstLtterPositionInTheAlphabet;
+                }
+                if (lastLetter >= 65 && lastLetter <= 90)
+                {
+                    int lastLtterPositionInTheAlphabet = lastLetter - 64;
+                    sum += result - lastLtterPositionInTheAlphabet;
+                }
+                else
+                {
+                    int lastLtterPositionInTheAlphabet = lastLetter - 96;
+                    sum += result + lastLtterPositionInTheAlphabet;
+                }
+            }
+            Console.WriteLine($"{sum:f2}");
+        
+        
+        }
+    }
+}
